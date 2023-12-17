edeb45a33b7b7d4822cff57c36c223589d6efadb
arpitshiv <201b062@juetguna.in>
2023-12-17 23:06:34 +0530

    debuged the errors

--- book-store/packages/config/app.go
+++ book-store/packages/config/app.go
@@ -1,6 +1,6 @@
 package config
 import (
-"github. com/jinzhu/gorm"
+"github.com/jinzhu/gorm"
 _ "github.com/jinzhu/gorm/dialects/mysql")
 var (
 db * gorm.DB
@@ -11,5 +11,5 @@ if err !=nil{
 panic(err)}
 db = d
 }
-func GetDB () *gorm.DB{
+func GetDB() *gorm.DB{
 return db}
