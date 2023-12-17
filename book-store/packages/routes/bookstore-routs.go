e18de7f6d64c4fd63907ce82324dd9d1165280af
arpitshiv <201b062@juetguna.in>
2023-12-18 00:37:27 +0530

    final commit

--- book-store/packages/routes/bookstore-routs.go
+++ book-store/packages/routes/bookstore-routs.go
@@ -4,8 +4,8 @@ import(
 	"github.com/arpitshiv/golangproject/packages/controller"
 )
 var RegisterBookStoreRoutes = func (router *mux.Router){
-router.HandleFunc("/book/", controllers.CreateBook).Methods("POST" ) 
-router.HandleFunc("/book/", controllers.GetBook).Methods("GET" ) 
-router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods( "GET") 
-router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT" ) 
-router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")}
+router.HandleFunc("/book/", controller.CreateBook).Methods("POST" ) 
+router.HandleFunc("/book/", controller.GetBook).Methods("GET" ) 
+router.HandleFunc("/book/{bookId}", controller.GetBookById).Methods( "GET") 
+router.HandleFunc("/book/{bookId}", controller.UpdateBook).Methods("PUT" ) 
+router.HandleFunc("/book/{bookId}", controller.DeleteBook).Methods("DELETE")}
